1740a958956dd342cb97521649393907e98a1813
ilan <ilanaaprilloriram@gmail.com>
2025-03-19 14:28:31 +0200

    added changes for ssl secured socket to make connection secured, also changes in explanation doc and in gitignore

--- client_tcp.py
+++ client_tcp.py
@@ -2,6 +2,8 @@ import socket
 from typing import Final # makes my types be final without ability to change their type
 import yaml
 import time
+import ssl
+import certs
 
 
 class Client:
@@ -14,7 +16,7 @@ class Client:
     # here we will use ECHO Server that will always answer upon connect to it
     ############################################################################################
     def __init__(self, ip = None, port = None):
-        self.CLIENT: Final[str] = "CLIENT"
+        self.app: Final[str] = "CLIENT"
         # self.ip: Final[str] = "127.0.0.1" if not ip else ip
         # self.port: Final[int] = 8820 if not port else port
 
@@ -25,16 +27,17 @@ class Client:
 
         ip, port, max_retries, retry_delay = self.init()
         self.IP: Final[str] =  ip  # also possible to do: socket.gethostbyname(socket.gethostname())  # <---- this way we determine the local host address, this way -> we set it hard codded: "127.0.0.1" if not ip else ip
-        print(f"[{self.CLIENT}]: Using IP: {self.IP}")
+        print(f"[{self.app}]: app is executed using the next params: ")
+        print(f"[{self.app}]: IP: {self.IP}")
 
         self.PORT: Final[int] = port # also possible to do: 8820 if not port else port
-        print(f"[{self.CLIENT}]: Using PORT: {self.PORT}")
+        print(f"[{self.app}]: PORT: {self.PORT}")
 
         self.max_retries = max_retries
-        print(f"[{self.CLIENT}]: Max number of connection retries: {self.max_retries}")
+        print(f"[{self.app}]: Max number of connection retries: {self.max_retries}")
 
         self.retry_delay = retry_delay
-        print(f"[{self.CLIENT}]: Delay between retries: {self.retry_delay}")
+        print(f"[{self.app}]: Delay between retries: {self.retry_delay}")
 
         self.connect()
 
@@ -47,22 +50,45 @@ class Client:
                    config["client"]["retry_delay"]
 
     def connect(self):
-        # 1. create client socket - this operation has nothing to do with Server (it doesnt requires a Server be connected)
-        print(f"[{self.CLIENT}]: Creating the socket ...")
+        # 1. create client 'regular' socket - this operation has nothing to do with Server (it doesnt requires a Server be connected)
+        print(f"\n\n[{self.app}]: Creating the 'regular' socket ...")
         self.client_socket = socket.socket(socket.AF_INET,     # this means we use protocol IP (our socket will expect to connect between 2 IP addresses
                                            socket.SOCK_STREAM) # this means we use protocol TCP (in charge of reliable connection)
-        # 2. connect socket to server - !!! Server must be running before we run this operation !!!
+        print(f"[{self.app}]: 'regular' socket ... created")
+
+        # 2. creating a context object that holds all relevant settings and configurations related to SSL/TLS secured connection
+        print(f"[{self.app}]: Creating the secured SSL context (set of rules for secure connection) ...")
+        # context = ssl.create_default_context() # <--- if I do it this way, I actually tell client to accept any cert from server, while server will by default create self signed certificate that will be by default rejected by python ssl so I need tell Client that will be sent specific self signed cert from server and please deal only with this one
+        context = ssl.create_default_context(cafile="certs/ilana_cert_01.pem")# <--- if I do it this way, I actually tell client to verify specific self signed certificate
+        print(f"[{self.app}]: default SSL context ... created")
+
+        # since we use self-signed certificate, which is default, by default python ssl module will reject it so we must handle it:
+        # Disable hostname verification, it is good while testing but in production we must replace this with: True
+        context.check_hostname = False
+        # this way we say: allow only this cert: 'certs/ilana_cert_01.pem',  or we can use ssl.CERT_NONE to make Client not check certificate at all
+        context.load_verify_locations('certs/ilana_cert_01.pem')
+        # context.verify_mode = ssl.CERT_NONE # Accept any certificate, it is good while testing but in production we must replace this with: CERT_REQUIRED
+
+        print(f"[{self.app}]: Wrapping 'regular' socket with SSL")
+        self.client_socket = context.wrap_socket(self.client_socket,server_hostname=self.IP) # if we set flag: context.check_hostname = False Server IP will not be checked
+        print(f"[{self.app}]: socket is wrapped with TLS/SSL, TLS version is: {self.client_socket.version()}")
+
+        # the correct order is:
+        # 0. create ssl context
+        # 1. create 'regular' socket
+        # 2. wrap socket with ssl
         for connect_attempt in range(1, self.max_retries + 1):
+            print(f"[{self.app}]: Attempting to connect Client to Server [{connect_attempt}], ip: {self.IP}, port: {self.PORT} ...")
+            # 3. connect server - here socket already need to be ssl socket as Server side expects ssl socket !
             try:
-                print(f"[{self.CLIENT}]: Attempting to connect Server [{connect_attempt}], ip: {self.IP}, port: {self.PORT} ...")
                 self.client_socket.connect((self.IP, self.PORT))
-                print(f"[{self.CLIENT}]: Connected to the Server successffully !")
+                print(f"[{self.app}]: Connected to the Server successfully !")
                 break
             except (ConnectionRefusedError, socket.timeout):
-                print(f"[{self.CLIENT}]: Server not up yet, retrying in {self.retry_delay} seconds...")
+                print(f"[{self.app}]: Server not up yet, retrying in {self.retry_delay} seconds...")
                 time.sleep(self.retry_delay)
         else:
-            print(f"[{self.CLIENT}]: Failed to connect to the Server after {self.max_retries} attempts ###")
+            print(f"[{self.app}]: Failed to connect to the Server after {self.max_retries} attempts ###")
             exit(1)  # Exit if connection never succeeded
 
     def start(self):
@@ -80,41 +106,39 @@ class Client:
             message = input("Please enter message: ").rstrip()
             # check input
             if not message:
-                print(f"[{self.CLIENT}]: Empty message is ignored")
+                print(f"[{self.app}]: Empty message is ignored")
             else:
-            # SEND
                 self.send(message) # send any message to server (either 'q' or not, as message 'q' tels the server to finish)
                 if message == 'q':
                     return
-                # WAIT + RECEIVE
                 self.receive()
 
     def send(self, message):
-        # 3. actual sending of the data to the server
-        print(f"[{self.CLIENT}]: Sending message: {message} to Server ..")
+        # actual sending of the data to the server
+        print(f"[{self.app}]: Sending message: {message} to Server ..")
         self.client_socket.sendall(message.encode())
-        print(f"[{self.CLIENT}]: Message was sent")
+        print(f"[{self.app}]: Message was sent")
         self.connection_store.setdefault(self.index,[]).append(message)
 
     def receive(self):
         # Client waits to get the answer from the server
         # we need to define MAX bytes we allow to extract from the socket - here we say max 1024 bytes (1K) if will be less ok
-        print(f"[{self.CLIENT}]: Waiting for response from the server ...")
+        print(f"[{self.app}]: Waiting for response from the server ...")
         received_data = self.client_socket.recv(self.MAX_DATA_SIZE).decode()  # blocking operation, client will not send next message before he got respond to the current message
-        print(f"[{self.CLIENT}]: Received message from the server: <{received_data}>")
+        print(f"[{self.app}]: Received message from the server: <{received_data}>")
         self.connection_store.setdefault(self.index, []).append(received_data)
         self.index += 1
 
     def disconnect(self):
-        print(f"[{self.CLIENT}]: Closing the SOCKET (connection) ....")
+        print(f"[{self.app}]: Closing the SOCKET (connection) ....")
         self.client_socket.close()
-        print(f"[{self.CLIENT}]: SOCKET (connection) is closed")
+        print(f"[{self.app}]: SOCKET (connection) is closed")
 
     def print_sent_messages(self):
-        print(f"\n[{self.CLIENT}]: All the messages that were sent: Client -> Server\n"
+        print(f"\n[{self.app}]: All the messages that were sent: Client -> Server\n"
               f"--------------------------------------------------------------------")
         for index, messages_list in self.connection_store.items():
-            print(f"[{self.CLIENT}]: [{index}]: {messages_list}")
+            print(f"[{self.app}]: [{index}]: {messages_list}")
 
 # I added here a main just in case I wish to run the client directly and not from simpl_client_server_app.py
 if __name__ == '__main__':
--- server_tcp.py
+++ server_tcp.py
@@ -4,6 +4,8 @@ import queue
 import socket
 import yaml
 from typing import Final # makes my types be final without ability to change their type
+import ssl
+import certs
 
 
 class Server:
@@ -17,7 +19,7 @@ class Server:
     ############################################################################################
 
     def __init__(self):
-        self.SERVER: Final[str] = "SERVER"
+        self.app: Final[str] = "SERVER"
         # self.ip: Final[str] = "127.0.0.1" if not ip else ip
         # self.port: Final[int] = 8820 if not port else port
 
@@ -29,11 +31,12 @@ class Server:
         self.connection_store = {}                 # multiprocessing.Queue() <-- this is good when we used processes and not threads
 
         ip,port = self.init()
+        print(f"[{self.app}]: app is executed using the next params: ")
         self.IP: Final[str] = ip # also possible to do: socket.gethostbyname(socket.gethostname()) if not ip else ip  # <---- this way we determine the local host address, this way -> we set it hard codded: "127.0.0.1" if not ip else ip
-        print(f"[{self.SERVER}]: Using IP: {self.IP}")
+        print(f"[{self.app}]: IP: {self.IP}")
 
         self.PORT: Final[int] = port # also possible to do: 8820 if not port else port
-        print(f"[{self.SERVER}]: Using PORT: {self.PORT}")
+        print(f"[{self.app}]: PORT: {self.PORT}")
 
     def init(self):
         with open("server_config.yaml", "r") as yaml_file:
@@ -48,40 +51,55 @@ class Server:
         :return: None
         """
         # 1. Create a socket object
-        print(f"[{self.SERVER}]: Creating the socket ...")
+        print(f"\n\n[{self.app}]: Creating the 'regular' socket ...")
         self.server_socket = socket.socket(socket.AF_INET,
-                                           socket.SOCK_STREAM) # Protocol: TCP
-        #self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+                                           socket.SOCK_STREAM) # use protocol: TCP
 
-        # 2. Bind the socket to an address and port
+        # 2. prepare secure context - setting up all the rules for secure communication
+        # It helps Python know how to handle encryption (TLS/SSL) for the server or client
+        # ssl.Purpose.CLIENT_AUTH tells python - hi I am server, and i wish to communicate securely with client/s
+        print(f"[{self.app}]: Creating the secured SSL context (set of rules for secure connection) ...")
+        context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+        # load certificate + key (certificate for authentication with Client, keys for encryption messages)
+        print(f"[{self.app}]: Load Authentication certificate and encryption keys, for secure connection ...")
+        context.load_cert_chain(certfile="certs/ilana_cert_01.pem",
+                                keyfile="certs/ilana_key_01.pem")
+
+        # 3. wrap regular server socket with SSL - from this moment all operations with socket, such as: Bind(), Listen(), Accept() wil be done with secured Server socket
+        # wrapping means => putting message in secured envelope. All the data sent/received through the socket is authenticated and encrypted
+        print(f"[{self.app}]: Wrapping the 'regular' socket to be SSL 'secured' socket ...")
+        self.server_socket = context.wrap_socket(self.server_socket,
+                                                 server_side=True) # <--- this line tells python that this is a Server and not a Client
+
+        # 4. Bind the socket (secured socket) to an address and port
         self.server_socket.bind((self.IP, self.PORT))
 
-        # 3. This method is actually puts Server's socket into listening mode, it is not blocking func
+        # 5. This method is actually puts Server's socket into listening mode, it is not blocking func
         # OS knows that only 1 connection is allowed, the rest will be rejected
         self.server_socket.listen(self.MAX_CONNECTIONS)
-        print(f"[{self.SERVER}]: Ready and is listening on port 8820...")
+        print(f"[{self.app}]: Ready and is listening on port 8820...")
 
         # make server no get stack waiting till client connects but check and keep running and vise versa
         # print(f"[{self.SERVER}]: configured not to stack and wait till the client is connected")
         # self.server_socket.setblocking(False)
 
-        # 4. Server is blocked (stack, pauses, waiting) till first Client (single client) connection. Server will wait forever for the connection
+        # 6. Server is blocked (stack, pauses, waiting) till first Client (single client) connection. Server will wait forever for the connection
         # first connected client will get the Server from stack, will be returned Client connection details: client_ip, client_socket (only socket actually in use)
         # then server will be stacked waiting for messages from connected client
         print(f"[SERVER]: is paused until client data is arrived")
         self.client_socket, client_address = self.server_socket.accept()
-        print(f"[{self.SERVER}]: Connection is established with client ip address: {client_address}, type: {type(self.client_socket)}")
+        print(f"[{self.app}]: Connection is established with client ip address: {client_address}, type: {type(self.client_socket)}")
 
-        # 5. create 2 different procs to handle receive and process of the messages from a client
-        print(f"[{self.SERVER}]: Creating 2 parallel server activities: receive_client_messages, process_client_messages ...")
+        # 7. create 2 different procs to handle receive and process of the messages from a client
+        print(f"[{self.app}]: Creating 2 parallel server activities: receive_client_messages, process_client_messages ...")
         # receiver_proc = multiprocessing.Process(target=self._receive_messages)#, args=(self.client_socket, self.client_messages_queue))
         # processor_proc = multiprocessing.Process(target=self._process_messages)#, args=(self. client_socket, self.client_messages_queue))
 
         receiver_thread = Thread(target=self._receive_messages)
         processor_thread = Thread(target=self._process_messages)
 
-        # 6. start the activities
-        print(f"[{self.SERVER}]: Starting these activities to run")
+        # 7. start the activities
+        print(f"[{self.app}]: Starting these activities to run")
         receiver_thread.start()
         processor_thread.start()
 
@@ -90,7 +108,7 @@ class Server:
 
         # receiver_thread.close()  # no need to close threads (only processes) threads shares memory while processes are not
         # processor_thread.close()
-        print(f"[{self.SERVER}]: Server shut down.")
+        print(f"[{self.app}]: Server shut down.")
 
     def _receive_messages(self):
         """
@@ -98,7 +116,7 @@ class Server:
         :return: None
         """
         while True:
-            print(f"[{self.SERVER}]: process 'receive & store' running ...")
+            print(f"[{self.app}]: process 'receive & store' running ...")
             try:
                 # Several scenarios can be here: --------------------------------------------------------------------------------------------------------------------
                 # 1: if no incoming message (from a client), server is stack (blocked) and keeps on waiting
@@ -110,18 +128,18 @@ class Server:
                 # --------------------------------------------------------------------------------------------------------------------------------------------------
                 data = self.client_socket.recv(self.MAX_DATA_SIZE) # its bad idea to do: data = self.client_socket.recv(self.MAX_DATA_SIZE).decode() as a data can be empty
                 if not data:
-                    print(f"[{self.SERVER}] empty message - is ignored")
+                    print(f"[{self.app}] empty message - is ignored")
                     continue
                     # break # consider here to close the DB
 
                 message_from_client = data.decode('utf-8')
-                print(f"[{self.SERVER}]: Received message from a Client: <{message_from_client}>")
+                print(f"[{self.app}]: Received message from a Client: <{message_from_client}>")
                 self.client_messages_queue.put(message_from_client)
 
                 if data.decode() == 'q':
                     break
             except ConnectionAbortedError as ee:
-                print(f"[{self.SERVER}]: ### Receive error: Client connection forcefully terminated, error:\n {ee} ###")
+                print(f"[{self.app}]: ### Receive error: Client connection forcefully terminated, error:\n {ee} ###")
                 break # consider here to close the DB
 
         # # 7. Server closes the connection - in any way either if client disconnected properly or if client's connection forcefully closed
@@ -142,7 +160,7 @@ class Server:
         index = 0
 
         while True:
-            print(f"[{self.SERVER}]: process 'retrieve & respond' running ...")
+            print(f"[{self.app}]: process 'retrieve & respond' running ...")
             try:
                 # .get() is for retrieve message from queue
                 # default it is blocking function but we can set a time parameter to limit the blocking time to 1 sec
@@ -153,42 +171,42 @@ class Server:
 
                 # check message, if empty then finish
                 if message == 'q':
-                    print(f"[{self.SERVER}]: extracted message = {message}, finish polling the queue")
+                    print(f"[{self.app}]: extracted message = {message}, finish polling the queue")
                     break  # consider here to close the DB
 
                 # respond to a client
                 resp_message = "Hello, client! I received your message."
-                print(f"[{self.SERVER}]: Sending response message back to client: {index}.{resp_message}")
+                print(f"[{self.app}]: Sending response message back to client: {index}.{resp_message}")
                 self.client_socket.sendall(resp_message.encode())
                 self.connection_store.setdefault(index, []).append(resp_message)
                 index += 1
-                print(f"[{self.SERVER}]: Message sent !")
+                print(f"[{self.app}]: Message sent !")
 
             except queue.Empty:
-                print(f"[{self.SERVER}]: yet found any message in queue, keep polling the queue ...")
+                print(f"[{self.app}]: yet found any message in queue, keep polling the queue ...")
                 continue
             except Exception as e:
-                print(f"[{self.SERVER}]: Queue processing error: {e} ###")
+                print(f"[{self.app}]: Queue processing error: {e} ###")
                 break
-        print(f"[{self.SERVER}]: processes finished !!!")
+        print(f"[{self.app}]: processes finished !!!")
 
     def disconnect(self):
         # 7. Server closes the connection - in any way either if client disconnected properly or if client's connection forcefully closed
         # If the server doesn’t call close(), the socket could remain in a "half-closed" state
         # where resources are still being held open even though the client is no longer connected.
         # Server MUST close Clients connection and his own Server connection according to the protocol
-        print(f"[{self.SERVER}]: Closing Client socket (connection) ")
+        print(f"[{self.app}]: Closing Client socket (connection) ")
         self.client_socket.close()
-        print(f"[{self.SERVER}]: Closing Server socket (connection) ")
+        print(f"[{self.app}]: Closing Server socket (connection) ")
         self.server_socket.close()
-        print(f"[{self.SERVER}]: processes finished !!!")
+        print(f"[{self.app}]: processes finished !!!")
 
     def print_received_messages(self):
-        print(f"\n[{self.SERVER}]: All the messages that were sent: Client -> Server\n"
+        print(f"\n[{self.app}]: All the messages that were sent: Client -> Server\n"
               f"--------------------------------------------------------------------")
         print(f"the len of the store is: {len(self.connection_store)}")
         for index, messages_list in self.connection_store.items():
-            print(f"[{self.SERVER}]: [{index}]: {messages_list}")
+            print(f"[{self.app}]: [{index}]: {messages_list}")
 
 # I added here a main just in case I wish to run the server directly and not from simpl_client_server_app.py
 if __name__ == '__main__':
